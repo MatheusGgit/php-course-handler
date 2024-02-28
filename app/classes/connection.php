476ebf55b91186da79f298224ac61d0deaa6a788
Matheus Garcia <garciamatheus27@gmail.com>
2024-02-28 10:59:09 -0300

    connection.php

--- app/classes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-class DBConnection {
-    public function __construct()
-    {
-        $db_path = __DIR__ . '/../database/banco_de_dados.db';
-        global $PDO;
-        $PDO = new PDO("sqlite:{$db_path}");
-    }
-}
